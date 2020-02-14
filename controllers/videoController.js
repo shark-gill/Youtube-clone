47eaa60a053147a8b00287d71163ce42f899147d
shark-gill <lhsrhk4@gmail.com>
2020-02-14 17:45:58 +0900

    uploadCreateVideo

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,14 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) => {
-  // const videos = await Video.find({});
-  // console.log(videos);
-  res.render("home", { pageTitle: "Home", videos: videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos: videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -21,12 +26,18 @@ export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, decription }
+    body: { title, description },
+    file: { path }
   } = req;
-  // To Do : Upload and save video
-  res.redirect(routes.videoDetail(123123123));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title: title,
+    description: description
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,7 @@
+import multer from "multer";
 import routes from "./routes";
+
+export const multerVideo = multer({ dest: "video/" });
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "SuTube";
@@ -9,3 +12,5 @@ export const localsMiddleware = (req, res, next) => {
   };
   next();
 };
+
+export const uploadVideo = multerVideo.single("videoFile");

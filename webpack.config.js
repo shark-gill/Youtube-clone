a985a8c289133a07fba7416b3532e1f90472bb98
shark-gill <lhsrhk4@gmail.com>
2020-02-17 19:58:16 +0900

    webpackSettingF

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,44 @@
 const path = require("path");
+const ExtractCSS = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-  entry: ENTRY_FILE,
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
+          { loader: "css-loader" },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins() {
+                return [autoprefixer({ browsers: "cover 99.5%" })];
+              }
+            }
+          },
+          { loader: "sass-loader" }
+        ])
+      }
+    ]
+  },
   output: {
     path: OUTPUT_DIR,
-    filename: "[name].[format]"
-  }
+    filename: "[name].js"
+  },
+  plugins: [new ExtractCSS("styles.css")]
 };
 
 module.exports = config;
